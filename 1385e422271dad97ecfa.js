2e01bd122db265d6cc20e3c350e8e0e30b8a25b4
DouglassY <teryakiboyzv2@gmail.com>
2025-05-26 17:01:54 +0300

    Deployment commit

---
+++ 1385e422271dad97ecfa.js
@@ -0,0 +1,4 @@
+import "./style.css";
+import firstLoad from "./functions/firstLoad";
+
+firstLoad();
--- src/functions/clearContent.js
+++
@@ -1,8 +0,0 @@
-const render = function () {
-  const content = document.querySelector("#content");
-  while (content.children.length > 0) {
-    content.children[0].remove();
-  }
-};
-
-export default render;
